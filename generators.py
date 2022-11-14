5e69c91bb80926d0973d57dccdd39020cb670413
DavidHao <32486280+david-yd-hao@users.noreply.github.com>
2022-11-14 03:51:40 +0000

    Updated Fine-tuned model and all outputs

--- generators.py
+++ generators.py
@@ -2,23 +2,60 @@ import os
 import openai
 import re
 
-key = "Your own key"
+
+def remove_numbers(str):
+    str = str.replace("1", "")
+    str = str.replace("2", "")
+    str = str.replace("3", "")
+    str = str.replace("4", "")
+    str = str.replace("5", "")
+    str = str.replace(".", "")
+    return str
 
 
-def text_generate(ingredients, api_key):
+def remove_hyphen(str):
+    str = str.replace("-", "")
+    return str
+
+
+def generate_names(ppt, api_key):
 
     openai.api_key = api_key
-    ingredients_str = ', '.join(ingredients)
-    prompt = 'Create three different food recipes while making use of ' + ingredients_str + '.' \
-             + 'Display weight of each ingredient used.' \
-             + 'Follow the format of \n Name: \n Ingredients: \n Instructions: \n'
+    prompt = ppt
+    print(prompt)
 
+    response = openai.Completion.create(
+            model="davinci:ft-oai-hackathon-2022-team-34-2022-11-14-01-27-55",
+            prompt=prompt,
+            temperature=0.0,
+            max_tokens=1024,
+            top_p=1,
+            best_of=3,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+
+    response_text = response['choices'][0]['text']
+    response_list = response_text.split('\n')[1:6]
+    response_list = [response.lstrip() for response in response_list]
+    response_list = [remove_numbers(response) for response in response_list]
+
+    return response_list
+
+
+def generate_all(ppt, api_key, finetune):
+    openai.api_key = api_key
+
+    prompt = ppt
+
+    print(prompt)
     # get response from GPT-3
+
     response = openai.Completion.create(
         model="text-davinci-002",
         prompt=prompt,
         temperature=0.0,
-        max_tokens=2048,
+        max_tokens=1024,
         top_p=1,
         best_of=3,
         frequency_penalty=0.0,
@@ -37,6 +74,7 @@ def text_generate(ingredients, api_key):
     # [first recipe:[ingredients needed in this dish:[amount, name]], second, third]
 
     ingredient_detail = []
+    count_unit = ["pound", "cup", "tablespoon", "teaspoon", "head", "lb", "tbsp", "cloves", "clove", "tsp", "tbsp.", "lb.", "bunch"]
     for item in ingredient_list:
         item_list = []
         ingredients = re.split('\n', item)[:-1]
@@ -45,10 +83,12 @@ def text_generate(ingredients, api_key):
                 ingredients.remove(ingre)
         for ingredient in ingredients:
             splitted = ingredient.split()
-            item_list.append([splitted[0] + ' ' + splitted[1], ' '.join(splitted[2:])])
+            if splitted[1] in count_unit:
+                item_list.append([remove_hyphen(splitted[0]), splitted[1], ' '.join(splitted[2:])])
+            else:
+                item_list.append([remove_hyphen(splitted[0]), ' '.join(splitted[1:])])
         ingredient_detail.append(item_list)
 
-    print(recipe_text)
     return name_list, ingredient_detail, instruction_list, recipe_text
 
 
@@ -68,9 +108,15 @@ def image_generate(recipe, api_key):
 
 
 if __name__ == '__main__':
-    name, ingredient, instruction, recipe = text_generate(['salmon', 'lettuce'], api_key=key)
-    print(image_generate(recipe[0], api_key=key))
+    key=''
+    prompt_1 = 'Name five low-carbon-footprint recipes that use chicken and basil'
+    prompt_1 = 'Name five low-carbon-footprint recipes that use salmon, mustard and broccoli'
+    prompt_1 = 'Name five low-carbon-footprint recipes that use beef'
+    name = generate_names(prompt_1, api_key=key)
 
 
-
+    prompt_2 = 'How to make' + name[4] + '. Display weight of each ingredient used.' \
+               + 'Follow the format of \n Name: \n Ingredients: \n Instructions: \n'
+    name, ingredient, instruction, recipe = generate_all(prompt_2, api_key=key, finetune=False)
+    print(ingredient)
 
---
+++ get_all_recipes.py
@@ -0,0 +1,69 @@
+name_salmon = [' Salmon and Broccoli Traybake with Mustard Sauce', 
+        ' Maple Mustard salmon with Roasted Broccoli', 
+        ' Salmon with Steamed Beans and Broccoli with mustard sauce', 
+        ' Baked Mustard-glazed Salmon with Tenderstem', 
+        ' Mustard Salmon With Crushed Potatoes And Tenderstem Broccoli']
+name_chicken = [' Chicken and Pesto Penne', 
+        ' Thai Basil Chicken', 
+        ' Chicken Stuffed with Basil and Mozzarella Cheese', 
+        ' Lemon Basil Chicken', 
+        ' Garlic Tomato Basil Chicken']
+name_beef = [' Thai Beef Salad', 
+            ' Beef Hamburger', 
+            ' Beef Curry with sweet pepper', 
+            ' Beef Stew with onions and mushrooms', 
+            ' Beef Steak']
+
+name_chicken_reduced = [' Thai Basil Chicken', 
+                        ' Lemon Basil Chicken', 
+                        ' Garlic Tomato Basil Chicken']
+
+name_beef_reduced = [' Thai Beef Salad', 
+                    ' Beef Hamburger', 
+                    ' Beef Curry with sweet pepper', 
+                    ' Beef Steak']
+
+beef_1 = [['1', 'pound', 'flank steak'], ['1/4', 'cup', 'fresh lime juice'], ['1/4', 'cup', 'fish sauce'], ['1', 'tablespoon', 'brown sugar'], ['1', 'tablespoon', 'vegetable oil'], ['1', 'teaspoon', 'grated ginger'], ['1', 'garlic clove, minced'], ['1/4', 'teaspoon', 'ground black pepper'], ['1/2', 'head', 'lettuce, shredded'], ['1/2', 'cucumber, sliced'], ['1/4', 'cup', 'fresh cilantro leaves'], ['1/4', 'cup', 'chopped peanuts']]
+
+beef_2 = [['1', 'pound', 'ground beef'], ['1/4', 'cup', 'onion, diced'], ['1', 'tablespoon', 'Worcestershire sauce'], ['1/4', 'teaspoon', 'salt']]
+
+beef_3 = [['1', 'pound', 'beef, cut into cubes'], ['1', 'onion, chopped'], ['1', 'green pepper, chopped'], ['1', 'tablespoon', 'curry powder'], ['1', 'can (14.5 ounces) diced tomatoes, undrained'], ['1', 'can (15 ounces) chickpeas, rinsed and drained'], ['1/2', 'cup', 'raisins'], ['1/2', 'cup', 'plain yogurt'], ['1/4', 'cup', 'chopped fresh cilantro']]
+
+beef_5 = [['1', 'pound', 'beef steak'], ['1', 'tablespoon', 'olive oil'], ['1', 'teaspoon', 'salt'], ['1/2', 'teaspoon', 'black pepper']]
+
+chicken_2 = [['1', 'lb', 'chicken breast, thinly sliced'], ['1/2', 'cup', 'oyster sauce'], ['1/4', 'cup', 'soy sauce'], ['1/4', 'cup', 'brown sugar'], ['1', 'tbsp', 'fish sauce'], ['1', 'tbsp', 'vegetable oil'], ['4', 'cloves', 'garlic, minced'], ['1/2', 'tsp', 'crushed red pepper flakes'], ['1/4', 'cup', 'chopped Thai basil']]
+
+chicken_4 = [['1', 'lb', 'chicken breast, cut into small strips'], ['1/4', 'cup', 'all-purpose flour'], ['2', 'tablespoons olive oil'], ['1/2', 'cup', 'chicken broth'], ['1/4', 'cup', 'lemon juice'], ['1', 'teaspoon', 'dried basil'], ['1/4', 'teaspoon', 'salt'], ['1/4', 'teaspoon', 'black pepper']]
+
+chicken_5 = [['1', 'chicken breast'], ['1/2', 'a tomato'], ['1/4', 'teaspoon', 'garlic powder'], ['1/4', 'teaspoon', 'basil']]
+
+salmon_1 = [['4', 'salmon fillets'], ['1', 'head', 'of broccoli, cut into florets'], ['1', 'tbsp. dijon mustard'], ['1', 'tbsp. honey'], ['1', 'tbsp. olive oil'], ['salt', 'and pepper']]
+
+salmon_2 = [['1', 'lb', 'salmon'], ['1/4', 'cup', 'Dijon mustard'], ['1/4', 'cup', 'maple syrup'], ['1/2', 'tsp', 'smoked paprika'], ['1/4', 'tsp', 'salt'], ['1/4', 'tsp', 'black pepper'], ['1', 'head', 'broccoli, cut into florets'], ['1', 'tbsp', 'olive oil'], ['1/4', 'tsp', 'salt'], ['1/4', 'tsp', 'black pepper']]
+
+salmon_3 = [['1', 'lb.', 'salmon'], ['1', 'cup', 'green beans'], ['1', 'cup', 'broccoli'], ['1/4', 'cup', 'Dijon mustard'], ['1/4', 'cup', 'white wine'], ['1/4', 'cup', 'olive oil'], ['salt', 'and pepper']]
+
+salmon_4 = [['4', 'salmon fillets'], ['2', 'tablespoon', 'Dijon mustard'], ['1', 'tablespoon', 'honey'], ['1', 'tablespoon', 'olive oil'], ['1', 'bunch', 'of Tenderstem broccoli'], ['Salt', 'and pepper']]
+
+salmon_5 = [['4', 'salmon fillets'], ['1/2', 'tablespoon', 'Dijon mustard'], ['1', 'tablespoon', 'honey'], ['1', 'tablespoon', 'olive oil'], ['Salt', 'and pepper'], ['1', 'pound', 'small Yukon Gold potatoes, halved'], ['1', 'bunch', 'tenderstem broccoli']]
+
+def get_recipe_ingredients(key):
+    keys = ["salmon", "chicken", "beef"]
+    assert key in keys
+    if key == "salmon":
+        ingredients = [salmon_1, salmon_2, salmon_3, salmon_4, salmon_5]
+        return name_salmon, ingredients
+    elif key == "chicken":
+        ingredients = [chicken_2, chicken_4, chicken_5]
+        return name_chicken_reduced, ingredients
+    elif key == "beef":
+        ingredients = [beef_1, beef_2, beef_3, beef_5]
+        return name_beef_reduced, ingredients
+    else:
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    name, ingredients = get_recipe_ingredients('beef')
+    print(name, len(name))
+    print(ingredients, len(ingredients))
